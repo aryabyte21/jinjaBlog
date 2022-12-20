aaae0d31c4c17982255aebdd2bf20f45921d3981
AryaArunB <arya2001bhosale@gmail.com>
2022-12-20 22:51:39 +0530

    initial commit

--- config.py
+++ config.py
@@ -4,14 +4,13 @@ basedir = os.path.abspath(os.path.dirname(__file__))
 
 class Config:
     SECRET_KEY = os.environ.get('SECRET_KEY') or 'hard to guess string'
-    MAIL_SERVER = os.environ.get('MAIL_SERVER', 'smtp.mailtrap.io')
-    MAIL_PORT = int(os.environ.get('MAIL_PORT', '2525'))
-    MAIL_USE_TLS = os.environ.get('MAIL_USE_TLS', 'true').lower() in \
-        ['true', 'on', '1']
-    MAIL_USERNAME = os.environ.get('MAIL_USERNAME')
-    MAIL_PASSWORD = os.environ.get('MAIL_PASSWORD')
+    MAIL_SERVER = 'smtp.gmail.com'
+    MAIL_PORT = '587'
+    MAIL_USE_TLS = 'true'
+    MAIL_USERNAME = 'aryaabyte@gmail.com'
+    MAIL_PASSWORD = 'hokiotnvohdqwwdf'
     FLASKY_MAIL_SUBJECT_PREFIX = '[Flasky]'
-    FLASKY_MAIL_SENDER = 'aryabyte@rediffmail.com'
+    FLASKY_MAIL_SENDER = 'aryaabyte@gmail.com'
     FLASKY_ADMIN = os.environ.get('FLASKY_ADMIN')
     SSL_REDIRECT = False
     SQLALCHEMY_TRACK_MODIFICATIONS = False
